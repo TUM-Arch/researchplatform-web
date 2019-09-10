452a8f1c9ee670208336a156aff478ac7bc8061d
Raymond Pinto <ge73lam@mytum.de>
2019-09-10 13:54:47 +0200

    Setup UI Error Dialog on Error Page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-function ErrorPage() {
-    return(
-        <div>
-            "Error Page"
-        </div>
-    );
+class ErrorPage extends React.Component {
+    render() {
+        return(
+            <div>
+                <Dialog
+                open="true"
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Error 404: Page not found"}</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                The requested page does not exist. Please try again later.
+                            </DialogContentText>
+                        </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => this.props.history.push('/main')} color="secondary">
+                            OK
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
 }
 
 export default ErrorPage;
