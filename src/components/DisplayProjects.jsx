fccbb40b885b499c37bdaf7288ec7fa8aa719f97
Karthik Narumanchi <karthiknr@gmail.com>
2019-09-18 14:42:53 +0200

    Projects displayed.

--- src/components/DisplayProjects.jsx
+++ src/components/DisplayProjects.jsx
@@ -8,14 +8,10 @@ class DisplayProjects extends React.Component {
     function 
     
     render() {
-        //TODO: Remove the below const and var
-        const globalUser = "raypinto"
+        
         const projects = this.props.projects;
         console.log(projects);
-        const viewProjects = this.props.viewProjects;
-        var renderedProjects=[];
-        var dummyYear;
-        let prevCreatedOn = projects[0].createdOn++;
+        let prevCreatedOn = projects[0].createdOn + 1;
 
         const styles = {
             TextStyle: {
@@ -29,38 +25,20 @@ class DisplayProjects extends React.Component {
                 paddingBottom: 10
             }
         }
-        
-        // projects.sort(function(a, b) {
-        //     return a.createdOn < b.createdOn ? 1 : -1 
-        // });
-        
-        // for(var i=0;i<projects.length;i++){
-        //     if (viewProjects === "all") {
-        //             if (projects[i].createdOn !== dummyYear) {
-        //                 dummyYear= projects[i].createdOn;
-        //                 renderedProjects.push(<Typography style={styles.TextStyle} color="secondary">{dummyYear}</Typography>);
-        //             }
-        //             renderedProjects.push(<Project id= {projects[i].id} name={ projects[i].name } desc= { projects[i].desc } dept={ projects[i].dept } createdBy= { projects[i].createdBy } createdOn = { projects[i].createdOn }  />);
-        //     } else {
-        //         if (globalUser === projects[i].createdBy){
-        //             if (projects[i].createdOn !== dummyYear) {
-        //                 dummyYear= projects[i].createdOn;
-        //                 renderedProjects.push(<Typography key={dummyYear} style={styles.TextStyle} color="secondary">{dummyYear}</Typography>);
-        //             }
-        //             renderedProjects.push(<Project key={i} id= {projects[i].id} name={ projects[i].name } desc= { projects[i].desc } dept={ projects[i].dept } createdBy= { projects[i].createdBy } createdOn = { projects[i].createdOn } />);
-        //         }
-        //     }
-        // }
+        function setPrevCreatedOn(createdOn) {
+            prevCreatedOn = createdOn;
+        }
         
         return (
             <div style={styles.divStyle}> 
-                { projects.map(({index, id, name,dept, desc, createdBy, createdOn}) => (
-                    <div>
-                        <Typography style={styles.TextStyle} color="secondary">{createdOn[index] < createdOn[index - 1]? createdOn : null}</Typography>
-                        <Project id= {id} name={name } desc= { desc } dept={ dept } createdBy= { createdBy } createdOn = { createdOn } />
-                        {/* {prevCreatedOn = createdOn} */}
-                    </div>
-                ))}
+                {projects.map(({id, name,dept, desc, createdBy, createdOn}) => (
+            <div>
+                <Typography style={styles.TextStyle} color="secondary">{createdOn < prevCreatedOn? createdOn : null}</Typography>
+                <Project id= {id} name={name } desc= { desc } dept={ dept } createdBy= { createdBy } createdOn = { createdOn } />
+                {setPrevCreatedOn(createdOn)}
+                
+            </div>
+        ))}
             </div>
         );
     }
--- src/reducers/mainPage.js
+++ src/reducers/mainPage.js
@@ -4,11 +4,13 @@ let initialState = {
   language: "en",
   viewProjects: "all",
   allProjects: [
-    {"id": 1, "name":"2nd SKIN Scaler", "desc": "The EU needs a zero-energy (ZE) refurbishment rate of around 3% to achieve its 2050 targets for a low carbon urban environment. The current refurbishment rate is far too low, despite all the policy measures over the past years. Although technically feasible, ZE-renovation projects developed in recent years are still too expensive, often suffer from disappointed inhabitants and unexpected higher energy-use.", "dept": ["Chair for building technology", "climate-friendly construction"], "createdBy": "pinto", "createdOn": "1994" }, 
-    {"id": 2, "name":"Building the buildings", "desc": "Although technically feasible, ZE-renovation projects developed in recent years are still too expensive, often suffer from disappointed inhabitants and unexpected higher energy-use.", "dept": ["Chair for building technology"], "createdBy": "raypinto", "createdOn": "2019" }, 
-    {"id": 3, "name":"Architects on Architects", "desc": "The new construction of the upper level building of the Waldorf School in Stuttgart was worked out in an integral planning process. Architects, building physicists, energy and HLSE planners worked together with planners of the other trades and in close cooperation with the client and the users to find a solution whose central goal was a sustainable building with a high quality of stay and a low demand for fossil energy Has. The findings from the parallel research project Development of exemplary new building measures for the Waldorf School Uhlandshöhe in Stuttgart on the basis of measurements and analyzes of newly built schools were consistently incorporated into the planning.", "dept": ["Chair of Urban Architecture", "Chair of Design and Design"], "prof": ["Prof. Dietrich Fink", "Prof. Prof. Uta Graff"], "createdBy": "raypinto", "createdOn": "2018" }
+    {"id": 1, "name":"2nd SKIN Scaler", "desc": "The EU needs a zero-energy (ZE) refurbishment rate of around 3% to achieve its 2050 targets for a low carbon urban environment. The current refurbishment rate is far too low, despite all the policy measures over the past years. Although technically feasible, ZE-renovation projects developed in recent years are still too expensive, often suffer from disappointed inhabitants and unexpected higher energy-use.", "dept": ["Chair for building technology", "climate-friendly construction"], "createdBy": "pinto", "createdOn": "2019" }, 
+    {"id": 2, "name":"Building the buildings", "desc": "Although technically feasible, ZE-renovation projects developed in recent years are still too expensive, often suffer from disappointed inhabitants and unexpected higher energy-use.", "dept": ["Chair for building technology"], "createdBy": "raypinto", "createdOn": "2018" }, 
+    {"id": 3, "name":"Architects on Architects", "desc": "The new construction of the upper level building of the Waldorf School in Stuttgart was worked out in an integral planning process. Architects, building physicists, energy and HLSE planners worked together with planners of the other trades and in close cooperation with the client and the users to find a solution whose central goal was a sustainable building with a high quality of stay and a low demand for fossil energy Has. The findings from the parallel research project Development of exemplary new building measures for the Waldorf School Uhlandshöhe in Stuttgart on the basis of measurements and analyzes of newly built schools were consistently incorporated into the planning.", "dept": ["Chair of Urban Architecture", "Chair of Design and Design"], "prof": ["Prof. Dietrich Fink", "Prof. Prof. Uta Graff"], "createdBy": "raypinto", "createdOn": "2017" },
+    {"id": 4, "name":"Architects on Architects", "desc": "The new construction of the upper level building of the Waldorf School in Stuttgart was worked out in an integral planning process. Architects, building physicists, energy and HLSE planners worked together with planners of the other trades and in close cooperation with the client and the users to find a solution whose central goal was a sustainable building with a high quality of stay and a low demand for fossil energy Has. The findings from the parallel research project Development of exemplary new building measures for the Waldorf School Uhlandshöhe in Stuttgart on the basis of measurements and analyzes of newly built schools were consistently incorporated into the planning.", "dept": ["Chair of Urban Architecture", "Chair of Design and Design"], "prof": ["Prof. Dietrich Fink", "Prof. Prof. Uta Graff"], "createdBy": "raypinto", "createdOn": "2017" }
   ],
-  myProjects: []
+  myProjects: [],
+  currentUser: "raypinto"
 }
 
 export default function mainPage(state = initialState, action) {
@@ -27,13 +29,13 @@ export default function mainPage(state = initialState, action) {
         return {
           ...state,
           viewProjects: "all",
-          projects: state.allProjects.sort(function(a, b) {return a.createdOn > b.createdOn ? 1 : -1 })
+          projects: state.allProjects.sort(function(a, b) {return a.createdOn < b.createdOn ? 1 : -1 })
         };
       case VIEWMY:
         return {
           ...state,
           viewProjects: "my",
-          myProjects: state.allProjects.filter(function(project) {return project.createdBy === "raypinto"}).sort(function(a, b) {return a.createdOn < b.createdOn ? 1 : -1 })
+          myProjects: state.allProjects.filter(function(project) {return project.createdBy === state.currentUser}).sort(function(a, b) {return a.createdOn < b.createdOn ? 1 : -1 })
         };
       default:
         return state
