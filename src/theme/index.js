dd8ce2c501c3f2553f9c44a5ed267653b69da914
Karthik Narumanchi <karthiknr@gmail.com>
2019-09-21 11:54:53 +0200

    Modify theme - Use system specific fonts

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,10 +3,21 @@ import {createMuiTheme} from "@material-ui/core/styles";
 const theme = createMuiTheme({
   palette: {
     primary: {main: "#ffffff", contrastText: "#000000"},
-    secondary: {main: "#3070b3", contrastText: "#ffffff"},
+    secondary: {main: "#306fb3", contrastText: "#ffffff"},
   },
-  status: {
-    danger: "orange",
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
   },
 });
 
