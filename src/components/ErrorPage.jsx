90c54d0c794f262f67742b6d7b75bc58d61602c8
Raymond Pinto <ge73lam@mytum.de>
2019-11-22 15:44:55 +0100

    Test commit

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -14,6 +14,7 @@ import {
 class ErrorPage extends React.Component {
   render() {
     const {history} = this.props;
+    console.log("Error");
     return (
       <div>
         <Dialog open={true}>
